9cb4c37237fd26909a65d7c7132aaa439dbf86b1
Mauro Rainis <mauro.rainis@gmail.com>
2019-09-07 22:28:58 +0200

    Aggiunto servizio e interfaccia

---
+++ Services/IStudentiService.cs
@@ -0,0 +1,13 @@
+﻿using CorsoBearziWebAppMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CorsoBearziWebAppMvc.Services
+{
+    public interface IStudentiService
+    {
+        List<Studente> GetAll();
+    }
+}
---
+++ Services/StudentiService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CorsoBearziWebAppMvc.Models;
+
+namespace CorsoBearziWebAppMvc.Services
+{
+    public class StudentiService : IStudentiService
+    {
+        public List<Studente> GetAll()
+        {
+            var lista = new List<Studente>
+            {
+                new Studente() { Nome = "Valentino", Cognome = "Rossi", Id = 1 },
+                new Studente() { Nome = "Marco", Cognome = "Pantani", Id = 2 },
+                new Studente() { Nome = "Alberto", Cognome = "Tomba", Id = 3 },
+                new Studente() { Nome = "Carolina", Cognome = "Kostner", Id = 4 },
+                new Studente() { Nome = "Valentina", Cognome = "Vezzali", Id = 5 },
+                new Studente() { Nome = "Elena", Cognome = "Cecchini", Id = 6 },
+                new Studente() { Nome = "Manuela", Cognome = "Di Centa", Id = 7 }
+            };
+            return lista;
+        }
+    }
+}
