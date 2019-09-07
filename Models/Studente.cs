0839524edce5644a3093314fc02b1bd2f67d2ee7
Mauro Rainis <mauro.rainis@gmail.com>
2019-09-07 22:28:24 +0200

    Aggiunto modello Studente

---
+++ Models/Studente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CorsoBearziWebAppMvc.Models
+{
+    public class Studente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+
+        public override string ToString()
+        {
+            return Cognome + " " + Nome;
+        }
+
+    }
+}
