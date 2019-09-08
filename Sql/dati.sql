6a89fdcfb515061bdb4ba4166762d2a74965b6d2
Mauro Rainis <mauro.rainis@gmail.com>
2019-09-08 09:06:32 +0200

    Aggiunto schema e dati demo per db

---
+++ Sql/dati.sql
@@ -0,0 +1,11 @@
+﻿insert into corsi values (1,'Programmazione in C#',2019);
+
+insert into studenti values (1,'Vittorio','C.',1);
+insert into studenti values (2,'Jeffrey','A.',1);
+insert into studenti values (3,'Diego','P.',1);
+insert into studenti values (4,'Massimiliano','F.',1);
+insert into studenti values (5,'Fabio','F.',1);
+insert into studenti values (6,'Luca','M.',1);
+insert into studenti values (7,'Matteo','P.',1);
+insert into studenti values (8,'Sebastiano','T.',1);
+insert into studenti values (9,'Riccardo','N.',1);
---
+++ Sql/schema.sql
@@ -0,0 +1,12 @@
+﻿create table corsi(
+id int not null primary key,
+nome varchar(256),
+anno int
+);
+
+create table studenti(
+id int not null primary key,
+nome varchar(32),
+cognome varchar(32),
+corso_id int references corsi(id)
+);
